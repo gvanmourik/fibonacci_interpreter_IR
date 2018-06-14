520a4b2e161a6076fbc5e138170356779ff7ecfc
Garrett Van Mourik <gvanmou@s1017129ca.local>
2018-06-14 10:13:58 -0700

    Third commit. Unoptimzed code working. The previous problem was caused by how the Function* was declared in InitFibonacciFnc()

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,20 +1,34 @@
+#include <llvm/ADT/APInt.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/IR/Argument.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/InstrTypes.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
 #include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Constants.h>
+#include <llvm/Support/Casting.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
-#include <llvm/ExecutionEngine/GenericValue.h>
-#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <algorithm>
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <vector>
 #include <iostream>
 
 using namespace llvm;
 
-
 Function* InitFibonacciFnc(LLVMContext &context, IRBuilder<> &builder, Module* module);
-CallInst* createCallInst(Argument *Arg, Value *v, std::string argName, BasicBlock *BB,
+CallInst* createSubCallInst(Argument *X, Value *constInt, std::string argName, BasicBlock *BB,
 							Function *fnc, std::string callName);
-
 
 int main(int argc, char* argv[])
 {
@@ -40,17 +54,18 @@ int main(int argc, char* argv[])
 	/// LLVM IR Variables
 	static LLVMContext context;
 	static IRBuilder<> builder(context);
-	std::unique_ptr<Module> module( new Module("fibonacciModule", context) );
-	// Module* module = new Module("fibonacciModule", context);
-	InitializeNativeTarget(); // for JIT compilation
+	std::unique_ptr<Module> mainModule( new Module("fibonacciModule", context) );
+	Module *module = mainModule.get();
+	InitializeNativeTarget();
+	InitializeNativeTargetAsmPrinter();
 
-	Function *fibFunc = InitFibonacciFnc( context, builder, module.get() );
+	Function *fibFunc = InitFibonacciFnc( context, builder, module );
 
 	/// Create a JIT
 	// auto engine = EngineKind::JIT;
 	std::string collectedErrors;
 	ExecutionEngine *exeEng = 
-		EngineBuilder(std::move(module))
+		EngineBuilder(std::move(mainModule))
 		.setErrorStr(&collectedErrors)
 		.setEngineKind(EngineKind::JIT)
 		.create();
@@ -66,7 +81,10 @@ int main(int argc, char* argv[])
 	Args[0].IntVal = APInt(32, targetFibNum);
 	GenericValue value = exeEng->runFunction(fibFunc, Args);
 
+	outs() << "\n" << *module;
+	outs() << "\n-----------------------------------------\n";
 	outs() << targetFibNum << "th fibonacci number = " << value.IntVal << "\n";
+	outs() << "-----------------------------------------\n";
 
 
 	return 0;
@@ -74,11 +92,15 @@ int main(int argc, char* argv[])
 
 Function* InitFibonacciFnc(LLVMContext &context, IRBuilder<> &builder, Module* module)
 {
-	std::vector<Type*> fncArgs;
-	fncArgs.push_back(builder.getInt8Ty()->getPointerTo());
-	ArrayRef<Type*> argsRef(fncArgs);
-	FunctionType *fncType = FunctionType::get(builder.getInt32Ty(), argsRef, false);
-	Function *fibFunc = Function::Create(fncType, Function::ExternalLinkage, "fibFunc", module);
+	// std::vector<Type*> fncArgs;
+	// fncArgs.push_back(builder.getInt8Ty()->getPointerTo());
+	// ArrayRef<Type*> argsRef(fncArgs);
+	// FunctionType *fncType = FunctionType::get(builder.getInt32Ty(), argsRef, false);
+	// Function *fibFunc = Function::Create(fncType, Function::ExternalLinkage, "fibFunc", module);
+	Function *fibFunc = cast<Function>(module->getOrInsertFunction("fibFunc", 
+																	Type::getInt32Ty(context),
+																	Type::getInt32Ty(context)
+																	));
 
 	Value* one = ConstantInt::get(builder.getInt32Ty(), 1);
 	Value* two = ConstantInt::get(builder.getInt32Ty(), 2);
@@ -87,32 +109,37 @@ Function* InitFibonacciFnc(LLVMContext &context, IRBuilder<> &builder, Module* m
 	BasicBlock *ContinueBB = BasicBlock::Create(context, "continue", fibFunc);
 	BasicBlock *ExitBB = BasicBlock::Create(context, "exit", fibFunc);
 	
-	Argument *IntArg = fibFunc->arg_begin();
-	IntArg->setName("FibIntArg");
+	Argument *X = &*fibFunc->arg_begin();
+	X->setName("X_Arg");
+	// Value* X_value = dyn_cast<ConstantInt>(X);
 
 	/// BASE case
+	Value *ifCond = new ICmpInst(*EntryBB, ICmpInst::ICMP_SLE, X, two, "if_cond");
+	BranchInst::Create(ExitBB, ContinueBB, ifCond, EntryBB);
+
 	// Return instruction
-	ReturnInst::Create(context, fib1, ExitBB);
+	ReturnInst::Create(context, one, ExitBB);
 	///
 
 	/// RECURSIVE case
-	CallInst *callFib1 = createCallInst(IntArg, one, "arg", ContinueBB,
+	CallInst *callFib1 = createSubCallInst(X, one, "", ContinueBB,
 										fibFunc, "fib1");
-	CallInst *callFib2 = createCallInst(IntArg, two, "arg", ContinueBB,
+	CallInst *callFib2 = createSubCallInst(X, two, "", ContinueBB,
 										fibFunc, "fib2");
-	Value *fibSum = BinaryOperator::CreateAdd(callFib1, callFib2, "fibSum", ContinueBB);
+	Value *sum = BinaryOperator::CreateAdd(callFib1, callFib2, "sum", ContinueBB);
+	
 	// Return instruction
-	ReturnInst::Create(context, fibSum, ContinueBB);
+	ReturnInst::Create(context, sum, ContinueBB);
 	///
 
 	return fibFunc;
 }
 
-CallInst* createCallInst(Argument *Arg, Value *v, std::string argName, BasicBlock *BB,
+CallInst* createSubCallInst(Argument *X, Value *constInt, std::string argName, BasicBlock *BB,
 							Function *fnc, std::string callName)
 {
-	Value *sub = BinaryOperator::CreateSub(Arg, v, argName, BB);
-	CallInst *call = CallInst::Create(fnc, sub, callName, BB);
+	Value *difference = BinaryOperator::CreateSub(X, constInt, argName, BB);
+	CallInst *call = CallInst::Create(fnc, difference, callName, BB);
 	call->setTailCall();
 
 	return call;
